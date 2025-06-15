881d457f14aeddc994e46ecdab6c61e0405f07d0
pauzhetsky <kazancev.alex2020@gmail.com>
2025-06-15 09:31:06 +0300

    progects change

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,8 +1,5 @@
 import project01 from './../img/projects/01.jpg'
-import project02 from './../img/projects/02.jpg'
 import project03 from './../img/projects/03.jpg'
-import project04 from './../img/projects/04.jpg'
-import project05 from './../img/projects/05.jpg'
 import project06 from './../img/projects/06.jpg'
 
 const projects = [
@@ -20,27 +17,6 @@ const projects = [
 		img: project03,
 		gitHubLink: 'https://pauzhetsky.github.io/MovieApp/',
 	},
-	// {
-	// 	id: 2,
-	// 	title: 'Todos',
-	// 	skills: 'HTML, CSS, TypeScript, ReactJS',
-	// 	img: project02,
-	// 	gitHubLink: 'https://todo-app-ten-mu-67.vercel.app/',
-	// },
-	// {
-	// 	id: 3,
-	// 	title: 'Toyota countdown',
-	// 	skills: 'HTML, CSS, JavaScript',
-	// 	img: project04,
-	// 	gitHubLink: 'https://pauzhetsky.github.io/ToyotaCountdown.ru/',
-	// },
-	// {
-	// 	id: 4,
-	// 	title: 'Background changer',
-	// 	skills: 'HTML, CSS, JavaScript',
-	// 	img: project05,
-	// 	gitHubLink: 'https://pauzhetsky.github.io/Background-changer/',
-	// },
 	{
 		id: 2,
 		title: 'Fashion',
