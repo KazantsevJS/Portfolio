1640d90df9226ec518a36f11b3726b3b2bcd8adf
pauzhetsky <kazancev.alex2020@gmail.com>
2025-06-17 15:47:54 +0300

    english

--- src/helpers/certificateList.js
+++ src/helpers/certificateList.js
@@ -1,41 +1,46 @@
 const certificateList = [
 	{
 		id: 0,
+		title: 'English level B2',
+		link: 'https://drive.google.com/file/d/1t2XN7LKhR976bECmym9WauqBXoy40Dcr/view?usp=drive_link',
+	},
+	{
+		id: 1,
 		title: 'Front End Development',
 		link: 'https://drive.google.com/file/d/1BWn8qJFyweDj7LwVh7B-DAHFCiyUcWbu/view?usp=sharing',
 	},
 	{
-		id: 1,
-		title: 'Databases',
-		link: 'https://drive.google.com/file/d/1s9MKDeCqBrbt5C5TPIuthUvAI3l-gKcI/view?usp=drive_link',
-	},
-	{
 		id: 2,
-		title: 'Data analysis',
-		link: 'https://drive.google.com/file/d/1pDhtjPD671ya_PQ5Xf149LqO6d4gaTmU/view?usp=drive_link',
-	},
-	{
-		id: 3,
 		title: 'Algorithms and data structures',
 		link: 'https://drive.google.com/file/d/1dV1-UxuodjkZK_0dyHaiaFdCV-oFrsPG/view?usp=drive_link',
 	},
 	{
+		id: 3,
+		title: 'Databases',
+		link: 'https://drive.google.com/file/d/1s9MKDeCqBrbt5C5TPIuthUvAI3l-gKcI/view?usp=drive_link',
+	},
+	{
 		id: 4,
+		title: 'Data analysis',
+		link: 'https://drive.google.com/file/d/1pDhtjPD671ya_PQ5Xf149LqO6d4gaTmU/view?usp=drive_link',
+	},
+	{
+		id: 5,
 		title: 'Cyber security',
 		link: 'https://drive.google.com/file/d/1MtkZoDIoWtHkulv_ynBsRsg477iU_ult/view?usp=drive_link',
 	},
 	{
-		id: 5,
+		id: 6,
 		title: 'Machine learning',
 		link: 'https://drive.google.com/file/d/1fueeGQYwkkAw4xYetTlY-Mmv9r8ut6C3/view?usp=drive_link',
 	},
 	{
-		id: 6,
+		id: 7,
 		title: 'Digital literacy',
 		link: 'https://drive.google.com/file/d/1fqvEKzTzVN4YUFzQoFFa2ud3j-ZY_Zkm/view?usp=drive_link',
 	},
 	{
-		id: 7,
+		id: 8,
 		title: 'Project and product management',
 		link: 'https://drive.google.com/file/d/1vzlUQDlyNkWyredeGpukz3Mzw_xpn2Rg/view?usp=drive_link',
 	},
