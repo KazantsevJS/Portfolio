8885745418e8fbe045eb53ab32088ff4597af5dd
pauzhetsky <kazancev.alex2020@gmail.com>
2025-06-19 12:19:48 +0300

    quiz

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -1,6 +1,8 @@
 import project01 from './../img/projects/01.jpg'
 import project03 from './../img/projects/03.jpg'
 import project06 from './../img/projects/06.jpg'
+import project10 from './../img/projects/10.jpg'
+import project11 from './../img/projects/11.jpg'
 
 const projects = [
 	{
@@ -22,6 +24,19 @@ const projects = [
 		title: 'Fashion',
 		skills: 'HTML, CSS, JavaScript, ReactJS',
 		img: project06,
+	},
+	{
+		id: 3,
+		title: 'LK Cloveri',
+		skills: 'HTML, CSS, JavaScript, VueJS',
+		img: project10,
+	},
+	{
+		id: 4,
+		title: 'Java Quiz',
+		skills: 'HTML, CSS, JavaScript, ReactJS',
+		img: project11,
+		gitHubLink: 'https://quiz-vert-eight.vercel.app/',
 	},
 ]
 
